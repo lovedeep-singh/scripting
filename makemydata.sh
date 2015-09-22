2e5377e239cef47e28b69a499adfded2f64ae983
lovedeep <bedilovedeeps@gmail.com>
2015-09-22 20:44:02 +0000

    change  the indention

--- makemydata.sh
+++ makemydata.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # this script creates a trivial directory with a few files to use as test data for the mybackup.sh script
-cd ~ mkdir mydata cp /etc/hosts mydata cp /etc/resolv.conf mydata cp /etc/motd mydata ln -s hosts mydata/hostslink
+cd ~ mkdir mydata 
+cp /etc/hosts mydata 
+cp /etc/resolv.conf mydata
+cp /etc/motd mydata
+ln -s hosts mydata/hostslink
+
 ls -l mydata
